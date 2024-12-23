af549c68f56e5c6d6e64d08859940254ff9c5db1
smanousi <lookoptica@otenet.gr>
2024-12-23 20:59:15 +0200

    trying to make changes

--- grip.js
+++ grip.js
@@ -2,12 +2,12 @@ const container = document.getElementById("container");
 function makeGrid(){
     var line = document.getElementById("container");
     for(let i = 0; i < 4; i++){
-        let row = document.createElement("tr");
+        let row = document.createElement("rows");
         row.id = "row" + i;
         line.appendChild(row);
         let rown = document.getElementById("row"+i);
         for (let j = 0; j < 4; j++){
-            let cell = document.createElement("td");
+            let cell = document.createElement("cells");
             cell.id = "cell" + j;
             rown.appendChild(cell);
         }
